a3a9b2ed3b2ef7049b314877bc9bd8f981fbc04b
Alonso <Alonso@Amarellos-MacBook-Pro.local>
2018-08-10 18:28:16 -0500

    inicio de campus

--- src/app/ejercicio/ejercicio.component.ts
+++ src/app/ejercicio/ejercicio.component.ts
@@ -9,27 +9,122 @@ export class EjercicioComponent implements OnInit {
 
   constructor() { }
 
-  secuencia = {digits:[0,0,0,0,0], reps:0}
+  secuencia = {
+    id: "",
+    reps: 0,
+    rung: "L",
+    steps: [],
+    left: "",
+    right: "",
+    start: {
+      left: "",
+      right: ""
+    }
+  }
   secuencias = []
+  rungs = ["1","2","3","4","5","6","7","8","9"]
+  history = []
 
   ngOnInit() {
   }
 
-  agregarDigito() {
-    this.secuencia.digits.push(0);
+  addStep(hand, step) {
+    if (!this.secuencia.start.left || !this.secuencia.start.right) {
+      this.secuencia.start[hand] = step;
+      this.secuencia[hand] = step;
+      return;
+    }
+    if (hand == 'double') {
+      this.secuencia.left = step;
+      this.secuencia.right = step;
+    }
+
+    this.secuencia.steps.push({hand: hand, step: step});
+    this.secuencia[hand] = step;
+  }
+
+  generarId(sec) {
+    let id = "";
+    id += sec.rung;
+    id += sec.start.left;
+    id += sec.start.right;
+    for (let i = 0; i < sec.steps.length; i++) {
+      id += sec.steps[i].hand;
+      id += sec.steps[i].step;
+    }
+    return id;
   }
 
   agregarSecuencia() {
+    this.secuencia.id = this.generarId(this.secuencia);
     this.secuencias.push(this.secuencia);
-    this.secuencia = {digits:[0]};
+    this.secuencia = {
+      id: "",
+      reps: 0,
+      rung: "L",
+      steps: [],
+      left: "",
+      right: "",
+      start: {
+        left: "",
+        right: ""
+      }
+    };
+  }
+
+  eliminarDraft() {
+    this.secuencia = {
+      id: "",
+      reps: 0,
+      rung: "L",
+      steps: [],
+      left: "",
+      right: "",
+      start: {
+        left: "",
+        right: ""
+      }
+    };
+  }
+
+  borrarSecuencia(secIndex) {
+    console.log(secIndex);
+    this.secuencias.splice(secIndex, 1);
   }
 
   changeReps(secIndex, factor) {
     this.secuencias[secIndex].reps += factor;
+    this.history.push(this.secuencias[secIndex].id);
+    console.log(this.history);
   }
 
-  logStuff () {    
-    console.info('Secuencia', this.secuencia);
+  duplicate(secIndex) {
+    let original = JSON.parse(JSON.stringify(this.secuencias[secIndex]));
+    let copy = JSON.parse(JSON.stringify(this.secuencias[secIndex]));
+    copy.left = original.right;
+    copy.right = original.left;
+    copy.start.left = original.start.right;
+    copy.start.right = original.start.left;
+
+    for (let i = 0; i < original.steps.length; i++) {
+      switch(original.steps[i].hand) {
+        case 'left':
+          copy.steps[i].hand = 'right';
+          break;
+        case 'right':
+          copy.steps[i].hand = 'left';
+          break;
+        default:
+          copy.steps[i].hand = 'double';
+      }
+    }
+    copy.id = this.generarId(copy);
+    this.secuencias.push(copy);
+  }
+
+  logStuff () {
+    console.info('Secuencia: ' , this.secuencia);
+    console.info('Secuencias: ', this.secuencias);
   }
 
 }
